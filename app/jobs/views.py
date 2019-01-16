77291cb8f8f8f67e7211e75dc6f70e00a38edc8d
Kanogaelias <kanogae@gmail.com>
2019-01-16 10:09:34 +0300

    add a request for job form and return list jobs

---
+++ app/jobs/views.py
@@ -0,0 +1,16 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from .forms import JobForm
+from .models import Job
+
+
+def create_job(request):
+    job_form = JobForm
+    if request.method == "POST":
+        job_form = job_form(request.POST)
+        if job_form.is_valid():
+            job_form.save()
+            messages.success(request, message="Job created successfully.")
+            return redirect(reverse("list-jobs"))
+    return render(request, "jobs/create_job.html", {"form": job_form})
