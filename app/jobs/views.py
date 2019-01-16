dd46fe7efaa7f2a15758ba4d4ab03e3a826e6d66
Kanogaelias <kanogae@gmail.com>
2019-01-16 10:45:28 +0300

    add request to update job and save job form

--- app/jobs/views.py
+++ app/jobs/views.py
@@ -25,3 +25,23 @@ def detail_job(request, pk):
     job = Job.objects.get(pk=pk)
     context = {"job": job}
     return render(request, "jobs/detail_job.html", context)
+
+def update_job(request, pk):
+    job = get_object_or_404(Job, pk=pk)
+    template_name = "jobs/update_job.html"
+    context = {}
+
+    if request.method == "GET":
+        context["form"] = JobForm(instance=job)
+        return render(request, template_name, context)
+
+    if request.method == "POST":
+        job_form = JobForm(request.POST, instance=job)
+        context["form"] = job_form
+        if job_form.is_valid():
+            job_form.save()
+            messages.success(request, message="Job updated successfully.")
+            return redirect(reverse("list-jobs"))
+        error_message = "Errors found. Please fill the form correctly."
+        messages.error(request, message=error_message)
+        return render(request, template_name, context)
